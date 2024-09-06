1043fd7c2df6ddb6a95aad4559ce3bd018fa6570
amorim66 <gabriel.amorim12@fatec.sp.gov.br>
2024-09-05 22:44:02 -0300

    git inicial

---
+++ dados.js
@@ -0,0 +1,64 @@
+let dados = [
+    {
+        titulo: "Áries",
+        descricao: "Áries é o primeiro signo do zodíaco e, como tal, carrega consigo a energia da primavera e o desejo de iniciar novos projetos. Pessoas nascidas sob este signo são conhecidas por sua impulsividade, paixão e coragem.",
+        link: "https://joaobidu.com.br/horoscopo-do-dia/horoscopo-do-dia-para-aries/"
+    },
+    {
+        titulo: "Touro",
+        descricao: "Touro é o segundo signo do zodíaco e é regido pelo elemento Terra. Pessoas nascidas sob este signo são conhecidas por sua determinação, praticidade e sensualidade.",
+        link: "https://joaobidu.com.br/horoscopo-do-dia/horoscopo-do-dia-para-touro/"
+    },
+    {
+        titulo: "Gêmeos",
+        descricao: "Gêmeos é o terceiro signo do zodíaco e é regido pelo elemento Ar. Pessoas nascidas sob este signo são conhecidas por sua curiosidade, versatilidade e habilidade comunicativa.",
+        link: "https://joaobidu.com.br/horoscopo-do-dia/horoscopo-do-dia-para-gemeos/"
+    },
+    {
+        titulo: "Câncer",
+        descricao: "Câncer é o quarto signo do zodíaco, regido pelo elemento Água. Pessoas nascidas sob este signo são conhecidas por sua sensibilidade, intuição e laços familiares fortes.",
+        link: "https://joaobidu.com.br/horoscopo-do-dia/horoscopo-do-dia-para-cancer/"
+    },
+    {
+        titulo: "Leão",
+        descricao: "Leão é o quinto signo do zodíaco, associado ao elemento Fogo. Pessoas nascidas sob este signo são conhecidas por sua confiança, criatividade e generosidade.",
+        link: "https://joaobidu.com.br/horoscopo-do-dia/horoscopo-do-dia-para-leao/"
+    },
+    {
+        titulo: "Virgem",
+        descricao: "Virgem é o sexto signo do zodíaco, regido pelo elemento Terra. Pessoas nascidas sob este signo são conhecidas por sua atenção aos detalhes, organização e praticidade.",
+        link: "https://joaobidu.com.br/horoscopo-do-dia/horoscopo-do-dia-para-virgem/"
+    },
+    {
+        titulo: "Libra",
+        descricao: "Libra é o sétimo signo do zodíaco e é regido pelo elemento Ar. Pessoas nascidas sob este signo são conhecidas por sua busca por equilíbrio, diplomacia e senso de justiça.",
+        link: "https://joaobidu.com.br/horoscopo-do-dia/horoscopo-do-dia-para-libra/"
+    },
+    {
+        titulo: "Escorpião",
+        descricao: "Escorpião é o oitavo signo do zodíaco, associado ao elemento Água. Pessoas nascidas sob este signo são conhecidas por sua intensidade, paixão e determinação.",
+        link: "https://joaobidu.com.br/horoscopo-do-dia/horoscopo-do-dia-para-escorpiao/"
+    },
+    {
+        titulo: "Sagitário",
+        descricao: "Sagitário é o nono signo do zodíaco e é regido pelo elemento Fogo. Pessoas nascidas sob este signo são conhecidas por seu otimismo, espírito aventureiro e desejo de liberdade.",
+        link: "https://joaobidu.com.br/horoscopo-do-dia/horoscopo-do-dia-para-sagitario/"
+    },
+    {
+        titulo: "Capricórnio",
+        descricao: "Capricórnio é o décimo signo do zodíaco, regido pelo elemento Terra. Pessoas nascidas sob este signo são conhecidas por sua disciplina, responsabilidade e ambição.",
+        link: "https://joaobidu.com.br/horoscopo-do-dia/horoscopo-do-dia-para-capricornio/"
+    },
+    {
+        titulo: "Aquário",
+        descricao: "Aquário é o décimo primeiro signo do zodíaco e é regido pelo elemento Ar. Pessoas nascidas sob este signo são conhecidas por sua originalidade, visão futurista e independência.",
+        link: "https://joaobidu.com.br/horoscopo-do-dia/horoscopo-do-dia-para-aquario/"
+    },
+    {
+        titulo: "Peixes",
+        descricao: "Peixes é o décimo segundo signo do zodíaco, associado ao elemento Água. Pessoas nascidas sob este signo são conhecidas por sua empatia, sensibilidade e intuição.",
+        link: "https://joaobidu.com.br/horoscopo-do-dia/horoscopo-do-dia-para-peixes/"
+    }
+];
+
+
