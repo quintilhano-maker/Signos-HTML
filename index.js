7d82d56ddc87fea3253443bb72f0e6bde9567330
quintilhano-maker <nataliaquintilhano11@gmail.com>
2024-09-15 20:57:28 -0300

    projeto convertido para node

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Servir arquivos estáticos da pasta 'public'
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
